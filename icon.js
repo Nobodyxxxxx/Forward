3d225ca1c50063e5aa80aa3761b94fd5f60a3cd0
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2025-06-05 00:31:54 +0800

    Update icon.js

--- icon.js
+++ icon.js
@@ -6,6 +6,6 @@
 	{"name":"SDL", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/SDL.png"},
 	{"name":"BB1", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/BB1.png"},
         {"name":"BB", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/BB.PNG"},
-        {"name":"BB", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/BB.PNG"},
+        {"name":"LL", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/LL.PNG"},
     ]
 }
