b5995066dfffe52e8f52e286a9f620a6925e1336
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2025-06-05 22:46:19 +0800

    Update icon.js

--- icon.js
+++ icon.js
@@ -1,12 +1,19 @@
 {
-"name":"Emby Icon", 
+"name":"Forward Icon", 
 "description":"by :)", 
 "icons":
     [
 	{"name":"SDL", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/SDL.png"},
         {"name":"BB", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/BB.png"},
-        {"name":"LL", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/LL.PNG"},
+        {"name":"levilde", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/levilde.pmg"},
         {"name":"rainbow", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/rainbow.png"},
-	{"name":"rainbow1", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/rainbow1.png"}
+	{"name":"melon", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/melon.png"}
+	{"name":"nebula", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/nebula.png"}
+        {"name":"QDZ", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/QDZ.png"}
+	{"name":"peach", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/peach.png"}
+	{"name":"kiku", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/kiku.png"}
+	{"name":"JSQ", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/JSQ.png"}
+	{"name":"youno", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/youno.png"}
+        {"name":"nyamedia", "url":"https://raw.githubusercontent.com/Nobodyxxxxx/Forward/refs/heads/main/icon/nyamedia.png"}
     ]
 }
